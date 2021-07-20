f3ab5f8222ef460e4b09b0333684a48dc0e9334a
JuYong Shin <macracube92@gmail.com>
2021-07-21 08:03:54 +0900

    boj10845

---
+++ boj/10845/10845.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int q[10000];
+int f, t;
+
+int size() {
+    return t - f;
+}
+
+int empty() {
+    return f == t;
+}
+
+int front() {
+    if (empty()) return -1;
+    return q[f];
+}
+
+int back() {
+    if (empty()) return -1;
+    return q[t-1];
+}
+
+void push(int x) {
+    q[t++] = x;
+}
+
+int pop() {
+    if (empty()) return -1;
+    return q[f++];
+}
+
+int main(void) {
+    int N, i, x;
+    char cmd[6];
+    scanf("%d", &N);
+    for (i=0; i<N; i++) {
+        scanf("%s", cmd);
+        if (cmd[0] == 'p') {
+            if (cmd[1] == 'u') {
+                scanf("%d", &x);
+                push(x);
+            } else printf("%d\n", pop());
+        } else if (cmd[0] == 's') printf("%d\n", size());
+        else if (cmd[0] == 'e') printf("%d\n", empty());
+        else if (cmd[0] == 'f') printf("%d\n", front());
+        else if (cmd[0] == 'b') printf("%d\n", back());
+    }
+    return 0;
+}
